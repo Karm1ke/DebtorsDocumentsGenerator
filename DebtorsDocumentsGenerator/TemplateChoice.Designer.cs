d2ed9dec220699beca3224c5492f0bce8ad6042d
Mike <mihail6666@gmail.com>
2022-12-12 22:51:00 +0500

    Добавлена форма выбора шаблона для формирования документов

---
+++ DebtorsDocumentsGenerator/TemplateChoice.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace DebtorsDocumentsGenerator
+{
+    partial class TemplateChoice
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.templates_dg = new System.Windows.Forms.DataGridView();
+            this.cancelButton = new DebtorsDocumentsGenerator.CustomButton();
+            this.generateButton = new DebtorsDocumentsGenerator.CustomButton();
+            this.processingStatusLbl = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.templates_dg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // templates_dg
+            // 
+            this.templates_dg.AllowUserToAddRows = false;
+            this.templates_dg.AllowUserToDeleteRows = false;
+            this.templates_dg.AllowUserToResizeRows = false;
+            this.templates_dg.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.templates_dg.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.templates_dg.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.templates_dg.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.templates_dg.Location = new System.Drawing.Point(12, 12);
+            this.templates_dg.MultiSelect = false;
+            this.templates_dg.Name = "templates_dg";
+            this.templates_dg.ReadOnly = true;
+            this.templates_dg.RowHeadersVisible = false;
+            this.templates_dg.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.templates_dg.Size = new System.Drawing.Size(321, 380);
+            this.templates_dg.TabIndex = 8;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cancelButton.Location = new System.Drawing.Point(85, 425);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(121, 30);
+            this.cancelButton.TabIndex = 7;
+            this.cancelButton.Text = "Отмена";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // generateButton
+            // 
+            this.generateButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.generateButton.Location = new System.Drawing.Point(212, 425);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(121, 30);
+            this.generateButton.TabIndex = 6;
+            this.generateButton.Text = "Старт";
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
+            // 
+            // processingStatusLbl
+            // 
+            this.processingStatusLbl.AutoSize = true;
+            this.processingStatusLbl.Location = new System.Drawing.Point(12, 395);
+            this.processingStatusLbl.Name = "processingStatusLbl";
+            this.processingStatusLbl.Size = new System.Drawing.Size(152, 13);
+            this.processingStatusLbl.TabIndex = 9;
+            this.processingStatusLbl.Text = "0 из 500 файлов обработано";
+            this.processingStatusLbl.Visible = false;
+            // 
+            // TemplateChoice
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 459);
+            this.Controls.Add(this.processingStatusLbl);
+            this.Controls.Add(this.templates_dg);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.generateButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "TemplateChoice";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Сформировать документ по шаблону";
+            this.Load += new System.EventHandler(this.TemplateChoice_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.templates_dg)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private CustomButton generateButton;
+        private CustomButton cancelButton;
+        private System.Windows.Forms.DataGridView templates_dg;
+        private System.Windows.Forms.Label processingStatusLbl;
+    }
+}
---
+++ DebtorsDocumentsGenerator/TemplateChoice.cs
@@ -0,0 +1,132 @@
+﻿using AdvancedFunctions;
+using DBWorkLB;
+using Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DebtorsDocumentsGenerator
+{
+    public partial class TemplateChoice : Form
+    {
+        public TemplateChoice(List<Debtor> debtors)
+        {
+            InitializeComponent();
+            InitBW();
+
+            this.debtors.AddRange(debtors);
+        }
+
+        BackgroundWorker generationBW = new BackgroundWorker();
+        BindingSource templatesListBS = new BindingSource();
+        List<Debtor> debtors = new List<Debtor>();
+
+        private void InitBW()
+        {
+            generationBW.WorkerSupportsCancellation = true;
+            generationBW.DoWork += new DoWorkEventHandler(DoWork);
+            generationBW.RunWorkerCompleted += new RunWorkerCompletedEventHandler(PostWork);
+        }
+
+        private void DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (e.Argument != null && e.Argument is Dictionary<string, string>)
+            {
+                var templates = (Dictionary<string, string>)e.Argument;
+                Action<int, int> updateLbl = new Action<int, int>((counts, maxCount) => {
+                    string lblText = $"{counts} файлов из {maxCount} обработано";
+                    if (processingStatusLbl.InvokeRequired)
+                    {
+                        processingStatusLbl.Invoke((MethodInvoker)delegate { processingStatusLbl.Text = lblText; });
+                    }
+                    else
+                    {
+                        processingStatusLbl.Text = lblText;
+                    }
+                });
+                var result = WordOperations.CreateDocumentsByTemplate(debtors, templates, updateLbl);
+                if (result)
+                {
+                    e.Result = true;
+                }
+            }
+        }
+
+        private void PostWork(object sender, RunWorkerCompletedEventArgs e)
+        {
+            processingStatusLbl.Visible = false;
+            generateButton.Enabled = true;
+            if (e.Result != null && (bool)e.Result)
+            {
+                InformOperations.setDisplayMessage("Документы успешно сформированы", "Формирование документов", ToolTipIcon.Info);
+                this.Close();
+            }
+        }
+
+        private void TemplateChoice_Load(object sender, EventArgs e)
+        {
+            LoadDG();
+        }
+
+        private void LoadDG()
+        {
+            templatesListBS.DataSource = DBSource.templatesDt;
+            templates_dg.DataSource = templatesListBS;
+
+            templates_dg.Columns["template_id"].Visible = false;
+            templates_dg.Columns["title"].HeaderText = "Наименование";
+            templates_dg.Columns["header_filepath"].Visible = false;
+            templates_dg.Columns["main_filepath"].Visible = false;
+            templates_dg.Columns["sub_filepath"].Visible = false;
+            templates_dg.Columns["local"].Visible = false;
+            templates_dg.Columns["doc_type"].Visible = false;
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            generationBW.CancelAsync();
+            this.Close();
+        }
+
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (debtors.Count > 0 && templates_dg.SelectedRows.Count > 0 && !generationBW.IsBusy)
+                {
+                    var row = templates_dg.SelectedRows[0];
+                    var templates = new Dictionary<string, string>();
+                    var doctype = row.Cells["doc_type"].Value.ToString();
+
+                    if (doctype == "txt")
+                    {
+                        templates.Add("header", row.Cells["header_filepath"].Value.ToString());
+                    }
+                    templates.Add("main", row.Cells["main_filepath"].Value.ToString());
+                    if (doctype == "txt")
+                    {
+                        templates.Add("sub", row.Cells["sub_filepath"].Value.ToString());
+                    }
+                    templates.Add("doctype", doctype);
+
+                    InformOperations.setDisplayMessage("Выполняется формирование файлов", "Генератор документов", ToolTipIcon.Info);
+
+                    processingStatusLbl.Visible = true;
+                    generationBW.RunWorkerAsync(templates);
+                    generateButton.Enabled = false;
+                }
+            }
+            catch(Exception ex)
+            {
+                InformOperations.setDisplayMessage($"Ошибка при запуске генерации документов: {ex.Message}");
+                Logger.Write($"Ошибка при запуске генерации документов: {ex.Message}");
+            }
+        }
+    }
+}
