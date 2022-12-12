efa7a3e63108a5f7217da572f26c68a4f635763d
Mike <mihail6666@gmail.com>
2022-12-12 22:50:27 +0500

    Добавлена форма добавления информации о шаблоне в БД

---
+++ DebtorsDocumentsGenerator/CreateTemplateForm.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace DebtorsDocumentsGenerator
+{
+    partial class CreateTemplateForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.headerTextFileBox = new System.Windows.Forms.TextBox();
+            this.selectHeaderFileButton = new System.Windows.Forms.Button();
+            this.clearHeaderTextBoxLink = new System.Windows.Forms.LinkLabel();
+            this.clearMainTextBoxLink = new System.Windows.Forms.LinkLabel();
+            this.selectMainTextButton = new System.Windows.Forms.Button();
+            this.mainTextFileBox = new System.Windows.Forms.TextBox();
+            this.clearSubTextBoxButton = new System.Windows.Forms.LinkLabel();
+            this.selectSubButton = new System.Windows.Forms.Button();
+            this.subTextFileBox = new System.Windows.Forms.TextBox();
+            this.titleBox = new System.Windows.Forms.TextBox();
+            this.titleLbl = new System.Windows.Forms.Label();
+            this.headerLbl = new System.Windows.Forms.Label();
+            this.mainLbl = new System.Windows.Forms.Label();
+            this.subLbl = new System.Windows.Forms.Label();
+            this.cancelButton = new DebtorsDocumentsGenerator.CustomButton();
+            this.createButton = new DebtorsDocumentsGenerator.CustomButton();
+            this.localCheckBox = new System.Windows.Forms.CheckBox();
+            this.doctypeComboBox = new System.Windows.Forms.ComboBox();
+            this.doctypeLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // headerTextFileBox
+            // 
+            this.headerTextFileBox.Location = new System.Drawing.Point(105, 61);
+            this.headerTextFileBox.Name = "headerTextFileBox";
+            this.headerTextFileBox.Size = new System.Drawing.Size(203, 20);
+            this.headerTextFileBox.TabIndex = 1;
+            // 
+            // selectHeaderFileButton
+            // 
+            this.selectHeaderFileButton.Location = new System.Drawing.Point(314, 61);
+            this.selectHeaderFileButton.Name = "selectHeaderFileButton";
+            this.selectHeaderFileButton.Size = new System.Drawing.Size(41, 20);
+            this.selectHeaderFileButton.TabIndex = 6;
+            this.selectHeaderFileButton.Text = "...";
+            this.selectHeaderFileButton.UseVisualStyleBackColor = true;
+            this.selectHeaderFileButton.Click += new System.EventHandler(this.selectHeaderFileButton_Click);
+            // 
+            // clearHeaderTextBoxLink
+            // 
+            this.clearHeaderTextBoxLink.AutoSize = true;
+            this.clearHeaderTextBoxLink.Location = new System.Drawing.Point(361, 64);
+            this.clearHeaderTextBoxLink.Name = "clearHeaderTextBoxLink";
+            this.clearHeaderTextBoxLink.Size = new System.Drawing.Size(54, 13);
+            this.clearHeaderTextBoxLink.TabIndex = 9;
+            this.clearHeaderTextBoxLink.TabStop = true;
+            this.clearHeaderTextBoxLink.Text = "Очистить";
+            this.clearHeaderTextBoxLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.clearHeaderTextBoxLink_LinkClicked);
+            // 
+            // clearMainTextBoxLink
+            // 
+            this.clearMainTextBoxLink.AutoSize = true;
+            this.clearMainTextBoxLink.Location = new System.Drawing.Point(361, 87);
+            this.clearMainTextBoxLink.Name = "clearMainTextBoxLink";
+            this.clearMainTextBoxLink.Size = new System.Drawing.Size(54, 13);
+            this.clearMainTextBoxLink.TabIndex = 10;
+            this.clearMainTextBoxLink.TabStop = true;
+            this.clearMainTextBoxLink.Text = "Очистить";
+            this.clearMainTextBoxLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.clearMainTextBoxLink_LinkClicked);
+            // 
+            // selectMainTextButton
+            // 
+            this.selectMainTextButton.Location = new System.Drawing.Point(314, 87);
+            this.selectMainTextButton.Name = "selectMainTextButton";
+            this.selectMainTextButton.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.selectMainTextButton.Size = new System.Drawing.Size(41, 20);
+            this.selectMainTextButton.TabIndex = 7;
+            this.selectMainTextButton.Text = "...";
+            this.selectMainTextButton.UseVisualStyleBackColor = true;
+            this.selectMainTextButton.Click += new System.EventHandler(this.selectMainTextButton_Click);
+            // 
+            // mainTextFileBox
+            // 
+            this.mainTextFileBox.Location = new System.Drawing.Point(105, 87);
+            this.mainTextFileBox.Name = "mainTextFileBox";
+            this.mainTextFileBox.Size = new System.Drawing.Size(203, 20);
+            this.mainTextFileBox.TabIndex = 2;
+            // 
+            // clearSubTextBoxButton
+            // 
+            this.clearSubTextBoxButton.AutoSize = true;
+            this.clearSubTextBoxButton.Location = new System.Drawing.Point(361, 113);
+            this.clearSubTextBoxButton.Name = "clearSubTextBoxButton";
+            this.clearSubTextBoxButton.Size = new System.Drawing.Size(54, 13);
+            this.clearSubTextBoxButton.TabIndex = 11;
+            this.clearSubTextBoxButton.TabStop = true;
+            this.clearSubTextBoxButton.Text = "Очистить";
+            this.clearSubTextBoxButton.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.clearSubTextBoxButton_LinkClicked);
+            // 
+            // selectSubButton
+            // 
+            this.selectSubButton.Location = new System.Drawing.Point(314, 113);
+            this.selectSubButton.Name = "selectSubButton";
+            this.selectSubButton.Size = new System.Drawing.Size(41, 20);
+            this.selectSubButton.TabIndex = 8;
+            this.selectSubButton.Text = "...";
+            this.selectSubButton.UseVisualStyleBackColor = true;
+            this.selectSubButton.Click += new System.EventHandler(this.selectSubFileButton_Click);
+            // 
+            // subTextFileBox
+            // 
+            this.subTextFileBox.Location = new System.Drawing.Point(105, 113);
+            this.subTextFileBox.Name = "subTextFileBox";
+            this.subTextFileBox.Size = new System.Drawing.Size(203, 20);
+            this.subTextFileBox.TabIndex = 3;
+            // 
+            // titleBox
+            // 
+            this.titleBox.Location = new System.Drawing.Point(105, 35);
+            this.titleBox.Name = "titleBox";
+            this.titleBox.Size = new System.Drawing.Size(310, 20);
+            this.titleBox.TabIndex = 0;
+            // 
+            // titleLbl
+            // 
+            this.titleLbl.AutoSize = true;
+            this.titleLbl.Location = new System.Drawing.Point(12, 38);
+            this.titleLbl.Name = "titleLbl";
+            this.titleLbl.Size = new System.Drawing.Size(60, 13);
+            this.titleLbl.TabIndex = 12;
+            this.titleLbl.Text = "Название:";
+            // 
+            // headerLbl
+            // 
+            this.headerLbl.AutoSize = true;
+            this.headerLbl.Location = new System.Drawing.Point(12, 65);
+            this.headerLbl.Name = "headerLbl";
+            this.headerLbl.Size = new System.Drawing.Size(64, 13);
+            this.headerLbl.TabIndex = 13;
+            this.headerLbl.Text = "Заголовок:";
+            // 
+            // mainLbl
+            // 
+            this.mainLbl.AutoSize = true;
+            this.mainLbl.Location = new System.Drawing.Point(12, 91);
+            this.mainLbl.Name = "mainLbl";
+            this.mainLbl.Size = new System.Drawing.Size(87, 13);
+            this.mainLbl.TabIndex = 14;
+            this.mainLbl.Text = "Основной блок:";
+            // 
+            // subLbl
+            // 
+            this.subLbl.AutoSize = true;
+            this.subLbl.Location = new System.Drawing.Point(12, 120);
+            this.subLbl.Name = "subLbl";
+            this.subLbl.Size = new System.Drawing.Size(51, 13);
+            this.subLbl.TabIndex = 15;
+            this.subLbl.Text = "Подпись";
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cancelButton.Location = new System.Drawing.Point(167, 168);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(121, 30);
+            this.cancelButton.TabIndex = 4;
+            this.cancelButton.Text = "Отмена";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // createButton
+            // 
+            this.createButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.createButton.Location = new System.Drawing.Point(294, 167);
+            this.createButton.Name = "createButton";
+            this.createButton.Size = new System.Drawing.Size(121, 30);
+            this.createButton.TabIndex = 5;
+            this.createButton.Text = "Сохранить";
+            this.createButton.UseVisualStyleBackColor = true;
+            this.createButton.Click += new System.EventHandler(this.createButton_Click);
+            // 
+            // localCheckBox
+            // 
+            this.localCheckBox.AutoSize = true;
+            this.localCheckBox.Location = new System.Drawing.Point(12, 147);
+            this.localCheckBox.Name = "localCheckBox";
+            this.localCheckBox.Size = new System.Drawing.Size(121, 17);
+            this.localCheckBox.TabIndex = 16;
+            this.localCheckBox.Text = "Локальный доступ";
+            this.localCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // doctypeComboBox
+            // 
+            this.doctypeComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.doctypeComboBox.FormattingEnabled = true;
+            this.doctypeComboBox.Items.AddRange(new object[] {
+            "Документ Word",
+            "Текстовыe файлы+Изображение"});
+            this.doctypeComboBox.Location = new System.Drawing.Point(105, 8);
+            this.doctypeComboBox.Name = "doctypeComboBox";
+            this.doctypeComboBox.Size = new System.Drawing.Size(310, 21);
+            this.doctypeComboBox.TabIndex = 17;
+            this.doctypeComboBox.SelectedIndexChanged += new System.EventHandler(this.doctypeComboBox_SelectedIndexChanged);
+            // 
+            // doctypeLbl
+            // 
+            this.doctypeLbl.AutoSize = true;
+            this.doctypeLbl.Location = new System.Drawing.Point(12, 11);
+            this.doctypeLbl.Name = "doctypeLbl";
+            this.doctypeLbl.Size = new System.Drawing.Size(29, 13);
+            this.doctypeLbl.TabIndex = 18;
+            this.doctypeLbl.Text = "Тип:";
+            // 
+            // CreateTemplateForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 210);
+            this.Controls.Add(this.doctypeLbl);
+            this.Controls.Add(this.doctypeComboBox);
+            this.Controls.Add(this.localCheckBox);
+            this.Controls.Add(this.subLbl);
+            this.Controls.Add(this.mainLbl);
+            this.Controls.Add(this.headerLbl);
+            this.Controls.Add(this.titleLbl);
+            this.Controls.Add(this.titleBox);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.createButton);
+            this.Controls.Add(this.clearSubTextBoxButton);
+            this.Controls.Add(this.selectSubButton);
+            this.Controls.Add(this.subTextFileBox);
+            this.Controls.Add(this.clearMainTextBoxLink);
+            this.Controls.Add(this.selectMainTextButton);
+            this.Controls.Add(this.mainTextFileBox);
+            this.Controls.Add(this.clearHeaderTextBoxLink);
+            this.Controls.Add(this.selectHeaderFileButton);
+            this.Controls.Add(this.headerTextFileBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "CreateTemplateForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Добавление шаблона";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox headerTextFileBox;
+        private System.Windows.Forms.Button selectHeaderFileButton;
+        private System.Windows.Forms.LinkLabel clearHeaderTextBoxLink;
+        private System.Windows.Forms.LinkLabel clearMainTextBoxLink;
+        private System.Windows.Forms.Button selectMainTextButton;
+        private System.Windows.Forms.TextBox mainTextFileBox;
+        private System.Windows.Forms.LinkLabel clearSubTextBoxButton;
+        private System.Windows.Forms.Button selectSubButton;
+        private System.Windows.Forms.TextBox subTextFileBox;
+        private CustomButton cancelButton;
+        private CustomButton createButton;
+        private System.Windows.Forms.TextBox titleBox;
+        private System.Windows.Forms.Label titleLbl;
+        private System.Windows.Forms.Label headerLbl;
+        private System.Windows.Forms.Label mainLbl;
+        private System.Windows.Forms.Label subLbl;
+        private System.Windows.Forms.CheckBox localCheckBox;
+        private System.Windows.Forms.ComboBox doctypeComboBox;
+        private System.Windows.Forms.Label doctypeLbl;
+    }
+}
---
+++ DebtorsDocumentsGenerator/CreateTemplateForm.cs
@@ -0,0 +1,257 @@
+﻿using AdvancedFunctions;
+using DBWorkLB;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DebtorsDocumentsGenerator
+{
+    public partial class CreateTemplateForm : Form
+    {
+        class WorkerArguments
+        {
+            public string title { get; set; }
+            public string ftpHeaderFileName { get; set; }
+            public string ftpMainFileName { get; set; }
+            public string ftpSubFileName { get; set; }
+            public bool local { get; set; }
+            public int typeIndex { get; set; }
+        }
+
+        public CreateTemplateForm()
+        {
+            InitializeComponent();
+            InitBW();
+
+            doctypeComboBox.SelectedIndex = 0;
+        }
+
+        BackgroundWorker creationBW = new BackgroundWorker();
+
+        private void InitBW()
+        {
+            creationBW.RunWorkerCompleted += new RunWorkerCompletedEventHandler(PostWork);
+            creationBW.WorkerSupportsCancellation = true;
+            creationBW.DoWork += new DoWorkEventHandler(DoWork);
+        }
+
+        private void DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (e.Argument != null && e.Argument is WorkerArguments)
+            {
+                var args = (WorkerArguments)e.Argument;
+                string headerPath = "";
+                string mainPath = "";
+                string subPath = "";
+                if (!args.local)
+                {
+                    var ftpResults = UploadFilesToFTP(
+                        args.ftpHeaderFileName,
+                        args.ftpMainFileName,
+                        args.ftpSubFileName);
+                    if (ftpResults.Count > 0)
+                    {
+                        headerPath = ftpResults["header"];
+                        mainPath = ftpResults["main"];
+                        subPath = ftpResults["sub"];
+                    }
+                }
+                else
+                {
+                    headerPath = args.ftpHeaderFileName;
+                    mainPath = args.ftpMainFileName;
+                    subPath = args.ftpSubFileName;
+                }
+                var dbResult = AddDBRecord(
+                    args.title,
+                    headerPath.Replace("\\", "\\\\"),
+                    mainPath.Replace("\\", "\\\\"),
+                    subPath.Replace("\\", "\\\\"),
+                    args.local,
+                    args.typeIndex);
+                if (dbResult)
+                {
+                    e.Result = true;
+                }
+            }
+        }
+
+        private void PostWork(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Result != null && (bool)e.Result)
+            {
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
+        }
+
+        private Dictionary<string, string> UploadFilesToFTP(string ftpHeaderFileName, string ftpMainFileName, string ftpSubFileName)
+        {
+            Dictionary<string, string> ftpFinalValues = new Dictionary<string, string>();
+            ftpFinalValues.Add("header", "");
+            ftpFinalValues.Add("main", "");
+            ftpFinalValues.Add("sub", "");
+            try
+            {
+                string ftpHeaderFilepath = "";
+                string ftpMainFilepath = "";
+                string ftpSubFilepath = "";
+                FTP.UploadFile(new string[] { ftpHeaderFileName, "" }, ref ftpHeaderFilepath);
+                ftpFinalValues["header"] = ftpHeaderFilepath;
+                FTP.UploadFile(new string[] { ftpMainFileName, "" }, ref ftpMainFilepath);
+                ftpFinalValues["main"] = ftpMainFilepath;
+                FTP.UploadFile(new string[] { ftpSubFileName, "" }, ref ftpSubFilepath);
+                ftpFinalValues["sub"] = ftpSubFilepath;
+            }
+            catch(Exception ex)
+            {
+                InformOperations.setDisplayMessage($"Ошибка при загрузке файлов на FTP: {ex.Message}");
+            }
+            return ftpFinalValues;
+        }
+
+        private bool AddDBRecord(
+            string title,
+            string ftpHeaderFilepath, 
+            string ftpMainFilepath, 
+            string ftpSubFilepath,
+            bool local,
+            int comboboxIndex)
+        {
+            try
+            {
+                var request = $@"
+                INSERT INTO templates(title, header_filepath, main_filepath, sub_filepath, local, doc_type) 
+                VALUES(
+                '{title}', 
+                '{ftpHeaderFilepath}', 
+                '{ftpMainFilepath}', 
+                '{ftpSubFilepath}',
+                {(local ? 1 : 0)},
+                '{(comboboxIndex == 0 ? "docx" : "txt")}');";
+                var result = DBOperations.queryNoneResult(request);
+                return result;
+            }
+            catch(Exception ex)
+            {
+                InformOperations.setDisplayMessage($"Ошибка при добавлении информации о шаблоне: {ex.Message}");
+            }
+            return false;
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void clearHeaderTextBoxLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            headerTextFileBox.Clear();
+        }
+
+        private void clearMainTextBoxLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            mainTextFileBox.Clear();
+        }
+
+        private void clearSubTextBoxButton_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            subTextFileBox.Clear();
+        }
+
+        private string GetFilePath(string type)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            switch (type)
+            {
+                case "text":
+                    ofd.Filter = "(*.txt;)|*.txt";
+                    break;
+                case "image":
+                    ofd.Filter = "(*.jpg;*.png;*.jpeg)|*.jpg;*.png;*.jpeg";
+                    break;
+                case "word":
+                    ofd.Filter = "(*.doc;*docx)|*.doc;*docx";
+                    break;
+            }
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                return ofd.FileName;
+            }
+            return "";
+        }
+
+        private void selectHeaderFileButton_Click(object sender, EventArgs e)
+        {
+            string path = GetFilePath("text");
+            headerTextFileBox.Text = path;
+        }
+
+        private void selectMainTextButton_Click(object sender, EventArgs e)
+        {
+            switch (doctypeComboBox.SelectedIndex)
+            {
+                case 0:
+                    mainTextFileBox.Text = GetFilePath("word");
+                    break;
+                case 1:
+                    mainTextFileBox.Text = GetFilePath("text");
+                    break;
+            }
+        }
+
+        private void selectSubFileButton_Click(object sender, EventArgs e)
+        {
+            string path = GetFilePath("image");
+            subTextFileBox.Text = path;
+        }
+
+        private void createButton_Click(object sender, EventArgs e)
+        {
+            creationBW.RunWorkerAsync(new WorkerArguments() { 
+                title = titleBox.Text,
+                ftpHeaderFileName = headerTextFileBox.Text,
+                ftpMainFileName = mainTextFileBox.Text,
+                ftpSubFileName = subTextFileBox.Text,
+                local = localCheckBox.Checked,
+                typeIndex = doctypeComboBox.SelectedIndex
+            });
+        }
+
+        private void doctypeComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (doctypeComboBox.SelectedIndex)
+            {
+                case 0:
+                    headerLbl.Visible = false;
+                    headerTextFileBox.Visible = false;
+                    selectHeaderFileButton.Visible = false;
+                    clearHeaderTextBoxLink.Visible = false;
+
+                    subLbl.Visible = false;
+                    subTextFileBox.Visible = false;
+                    selectSubButton.Visible = false;
+                    clearSubTextBoxButton.Visible = false;
+                    break;
+                case 1:
+                    headerLbl.Visible = true;
+                    headerTextFileBox.Visible = true;
+                    selectHeaderFileButton.Visible = true;
+                    clearHeaderTextBoxLink.Visible = true;
+
+                    subLbl.Visible = true;
+                    subTextFileBox.Visible = true;
+                    selectSubButton.Visible = true;
+                    clearSubTextBoxButton.Visible = true;
+                    break;
+            }
+        }
+    }
+}
